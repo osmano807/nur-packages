3cb5719bd003a127665f19a5894f55cf19892237
Joaquim Simão <osmano807@gmail.com>
2024-08-20 21:19:23 -0300

    fix(rust): 🐛 Update diesel-cli package to use specific rustPlatform
    Update the diesel-cli package in cells/rust/apps.nix to use rustPlatform from rustPackages_1_79, fixing compilation with MSRV 1.78

--- cells/rust/apps.nix
+++ cells/rust/apps.nix
@@ -6,5 +6,5 @@
 in {
   leptosfmt = nixpkgs.callPackage ./leptosfmt {};
 
-  diesel-cli = nixpkgs.callPackage ./diesel-cli/package.nix {};
+  diesel-cli = nixpkgs.callPackage ./diesel-cli/package.nix { inherit (nixpkgs.rustPackages_1_79) rustPlatform; };
 }
