618e213f3352a2bc14fa1e7a3fb56c0f921966e8
Joaquim Simão <osmano807@gmail.com>
2024-10-21 20:11:35 -0300

    feat: update leptosfmt to unstable
    Add a leptosfmt version including https://github.com/bram209/leptosfmt/pull/152

--- cells/rust/leptosfmt/default.nix
+++ cells/rust/leptosfmt/default.nix
@@ -1,19 +1,21 @@
 {
   lib,
   rustPlatform,
-  fetchCrate,
+  fetchFromGitHub,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "leptosfmt";
-  version = "0.1.32";
+  version = "unstable-2021-10-21";
 
-  src = fetchCrate {
-    inherit version;
-    crateName = pname;
-    hash = "sha256-9T510sSR/+qrTk2iOgBWWL3BPnT3bRls8Cjkxk6fYOk=";
+  src = fetchFromGitHub {
+    owner = "bram209";
+    repo = pname;
+    rev = "refs/pull/152/head";
+    fetchSubmodules = true;
+    hash = "sha256-wkv5eVXvHX6RYOTEui91Qeoopqjsc3xpP5jl0dIbTyk=";
   };
 
-  cargoHash = "sha256-MetYHwnjkPFr/Xj7EuPEsjjD4kBH2fEnFXIni1olqhU=";
+  cargoHash = "sha256-OXfluFh24nB4WvB5xtduEHDA18yO5SAeQyfGBYHVuN0=";
 
   meta = {
     description = "A formatter for the leptos view! macro";
