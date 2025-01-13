31f1b715437fd0ab391ad87b0cf3e894a0acf672
Joaquim Simão <osmano807@gmail.com>
2025-01-13 19:59:36 -0300

    feat: update wasm-bindgen-cli

--- cells/rust/wasm-bindgen-cli/default.nix
+++ cells/rust/wasm-bindgen-cli/default.nix
@@ -10,9 +10,9 @@
   stdenv,
   curl,
   darwin,
-  version ? "0.2.99",
-  hash ? "sha256-1AN2E9t/lZhbXdVznhTcniy+7ZzlaEp/gwLEAucs6EA=",
-  cargoHash ? "sha256-DbwAh8RJtW38LJp+J9Ht8fAROK9OabaJ85D9C/Vkve4=",
+  version ? "0.2.100",
+  hash ? "sha256-3RJzK7mkYFrs7C/WkhW9Rr4LdP5ofb2FdYGz1P7Uxog=",
+  cargoHash ? "sha256-tD0OY2PounRqsRiFh8Js5nyknQ809ZcHMvCOLrvYHRE=",
 }: let
   inherit
     (darwin.apple_sdk.frameworks)
