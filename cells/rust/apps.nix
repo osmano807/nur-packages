865f1c1bb2d20cfe7eb152c987247ca553559974
Joaquim Simão <osmano807@gmail.com>
2025-01-13 22:02:23 -0300

    feat: update rustPackages
    the old rustPackages_1_79 was removed from nixpkgs

--- cells/rust/apps.nix
+++ cells/rust/apps.nix
@@ -6,9 +6,9 @@
 in {
   leptosfmt = nixpkgs.callPackage ./leptosfmt {};
 
-  diesel-cli = nixpkgs.callPackage ./diesel-cli/package.nix {inherit (nixpkgs.rustPackages_1_79) rustPlatform;};
+  diesel-cli = nixpkgs.callPackage ./diesel-cli/package.nix {inherit (nixpkgs.rustPackages_1_80) rustPlatform;};
 
-  cargo-leptos = nixpkgs.callPackage ./cargo-leptos {inherit (nixpkgs.rustPackages_1_79) rustPlatform;};
+  cargo-leptos = nixpkgs.callPackage ./cargo-leptos {inherit (nixpkgs.rustPackages_1_80) rustPlatform;};
 
   wasm-bindgen-cli = nixpkgs.callPackage ./wasm-bindgen-cli {};
 }
