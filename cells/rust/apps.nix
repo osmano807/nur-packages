9dfa0953075a537b876179b2cef357879b4e752c
Joaquim Simão <osmano807@gmail.com>
2025-01-13 22:46:07 -0300

    feat: update rustPackages to use latest
    nixos 24.05 uses rust 1.82, so no need to fix it

--- cells/rust/apps.nix
+++ cells/rust/apps.nix
@@ -6,9 +6,9 @@
 in {
   leptosfmt = nixpkgs.callPackage ./leptosfmt {};
 
-  diesel-cli = nixpkgs.callPackage ./diesel-cli/package.nix {inherit (nixpkgs.rustPackages_1_80) rustPlatform;};
+  diesel-cli = nixpkgs.callPackage ./diesel-cli/package.nix {inherit (nixpkgs.rustPackages) rustPlatform;};
 
-  cargo-leptos = nixpkgs.callPackage ./cargo-leptos {inherit (nixpkgs.rustPackages_1_80) rustPlatform;};
+  cargo-leptos = nixpkgs.callPackage ./cargo-leptos {inherit (nixpkgs.rustPackages) rustPlatform;};
 
   wasm-bindgen-cli = nixpkgs.callPackage ./wasm-bindgen-cli {};
 }
