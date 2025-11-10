7864a3260ffe32c2444040f29207cb202f1574f7
Joaquim Simão <osmano807@gmail.com>
2025-11-09 22:29:21 -0300

    fix: remove nix input indirections
    I'm using the values according to osmano807/machine-config@aedb4dc38f5883878d605829a997e0080d894f69
    to reproduce the previous flake.nix and flake.lock values, as
    I'm getting some weird behaviour with indirect flake inputs

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   description = "My personal NUR repository";
 
   inputs = {
-    nixpkgs.url = "nixpkgs";
-    #nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-unstable.url = "latest";
+    nixpkgs.url = "github:NixOS/nixpkgs/086b448a5d54fd117f4dc2dee55c9f0ff461bdc1";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/99dc8785f6a0adac95f5e2ab05cc2e1bf666d172";
 
     std = {
-      url = "github:divnix/std";
+      url = "github:divnix/std/f8f6f70cdc9234d36d3d445d99a60b9267644df8";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.devshell.url = "github:numtide/devshell";
+      inputs.devshell.url = "github:numtide/devshell/67cce7359e4cd3c45296fb4aaf6a19e2a9c757ae";
     };
 
     flake-compat = {
